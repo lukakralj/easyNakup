4c236f99be126b88cf85f94a193837ff1362cb76
Danilo Del Busso <danilo.delbusso1@gmail.com>
2019-03-30 20:57:21 +0100

    idk

--- nodeServer/userRouter.js
+++ nodeServer/userRouter.js
@@ -4,17 +4,37 @@ const UserRouter = express.Router();
 const user_controller = require('./usercontroller');
 const User = user_controller.User;
 
+keys = ["123"]
+
 UserRouter.route('/add').post(function (req, res) {
-  const user = new User(req.body);
-  user.save()
-    .then(user => {
-      res.json('User added successfully');
-    })
-    .catch(err => {
-      res.status(400).send("unable to save to database");
-    });
+  if (isKeyValid(req.body.key)) {
+    const user = new User(req.body);
+    user.save()
+      .then(user => {
+        res.json('User added successfully');
+      })
+      .catch(err => {
+        res.status(400).send("unable to save to database");
+      });
+  }
+  else{
+    res.json("INVALID KEY")
+  }
 });
 
+function isKeyValid(key){
+  return keys.includes(key)
+}
+
+UserRouter.route('/addKey').post(function (req, res){
+  keys.push(req.body.key);
+  res.json("Added key "+req.body.key)
+});
+
+UserRouter.route('/deleteAllKeys').get(function (req, res){
+  keys = []
+  res.json("Removed all keys")
+});
 
 UserRouter.route('/removeall').post(function (req, res) {
   User.deleteMany({}, function (err, result) {
@@ -50,7 +70,6 @@ UserRouter.route('/getAll').get(function (req, res) {
 UserRouter.route('/remove').post(function (req, res) {
   console.log("removing order with id", req.body._id);
   try {
-
     User.findOne({ _id: req.body._id }, function (err, result) {
       result.remove((err) => {
         if (err) {
