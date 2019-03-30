2b99afce83a8a3fc1ddb1d861becc93c99626b95
lukakralj <luka.kralj2>
2019-03-30 16:53:10 +0000

    Fix alvaro's stuff

--- ocr/ProcessList.py
+++ ocr/ProcessList.py
@@ -2,9 +2,6 @@ from ListRecognition import get_list
 from ImageUpload import upload_image
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i',dest="image")
-img_path = parser.parse_args().image
+def process_list(path,name):
+    return list(get_list(upload_image(path,name)))
 
-def process_list(path):
-    return list(get_list(upload_image(path,'Hellooo')))
