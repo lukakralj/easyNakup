2da630dbad4bc360e64a85cf980907d55cfcb4fc
lukakralj <luka.kralj2@gmail.com>
2019-03-31 06:00:38 +0200

    idkx2

--- dragonboard/setup_scripts/GPIOLibrary.py
+++ dragonboard/setup_scripts/GPIOLibrary.py
@@ -1,4 +1,6 @@
+
 class GPIOProcessor:
+
     '''This is the GPIO Processor class.  IT is used to create GPIO objects and
     keep track of them. Pins 23-34 have the their corresponding GPIO number
     stored so that they may easily called with the appropriate getPin method.
@@ -11,6 +13,7 @@ class GPIOProcessor:
         '''The getPin method is used to create a GPIO object. After a GPIO is
         created it is added to a list to keep track of all GPIO's being used.
         '''
+
         pin = GPIO(pin_number)
         pin.openPin()
         self.GPIOList.append(pin)
@@ -57,51 +60,55 @@ class GPIOProcessor:
         uses GPIO's.  It changes the GPIO's back to inputs(which is safer when
         not in use) and then unexports all GPIO's that were used.
         '''
+
         for pin in self.GPIOList:
             pin.input()
             pin.closePin()
         self.GPIOList = []
 
-class GPIO:
-    '''This is the GPIO class.  This class contains methods that can be used to
-    control the GPIO's.  It is used by the GPIOProcessor class. These methods
-    open the appropriate file to export GPIOs or change direction and value. 
-    '''
-    global PATH
-    PATH = "/sys/class/gpio/"
 
-    def __init__(self,pin_number):
+class GPIO:
+
+    global PATH
+    PATH = '/sys/class/gpio/'
+
+    def __init__(self, pin_number):
         self.pin_number = pin_number
 
     def openPin(self):
-        file = open(PATH + "export",'w')
+        file = open(PATH + 'export', 'w')
         file.write(str(self.pin_number))
         file.close()
 
     def closePin(self):
-        file = open(PATH + "unexport",'w')
+        file = open(PATH + 'unexport', 'w')
         file.write(str(self.pin_number))
         file.close()
 
-    def setDirection(self,direction):
-	    print(PATH + "gpio" + str(self.pin_number) + "/direction")
-        file = open(PATH + "gpio" + str(self.pin_number) + "/direction",'w')
+    def setDirection(self, direction):
+        print PATH + 'gpio' + str(self.pin_number) + '/direction'
+
+        file = open(PATH + 'gpio' + str(self.pin_number) + '/direction'
+                    , 'w')
         file.write(str(direction))
         file.close()
 
-    def setValue(self,value):
-        file = open(PATH + "gpio" + str(self.pin_number) + "/value",'w')
+    def setValue(self, value):
+        file = open(PATH + 'gpio' + str(self.pin_number) + '/value', 'w'
+                    )
         file.write(str(value))
         file.close()
 
     def getDirection(self):
-        file = open(PATH + "gpio" + str(self.pin_number) + "/direction",'r')
+        file = open(PATH + 'gpio' + str(self.pin_number) + '/direction'
+                    , 'r')
         direction = file.read()
         file.close()
         return direction
 
     def getValue(self):
-        file = open(PATH + "gpio" + str(self.pin_number) + "/value",'r')
+        file = open(PATH + 'gpio' + str(self.pin_number) + '/value', 'r'
+                    )
         value = file.read()
         file.close()
         return int(value)
@@ -113,7 +120,7 @@ class GPIO:
         self.setValue(0)
 
     def input(self):
-        self.setDirection("in")
+        self.setDirection('in')
 
     def out(self):
-        self.setDirection("out")
+        self.setDirection('out')
